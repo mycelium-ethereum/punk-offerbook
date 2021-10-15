9061562a8e6efa1e5494f5bece4da9684d978502
Sumer Malhotra <sumermalhotra1998@gmail.com>
2021-10-15 16:42:42 +0800

    modify error msg, x defaults to 1

--- floor_price.py
+++ floor_price.py
@@ -5,10 +5,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/punkfloor")
-async def root(x: int):
+async def root(x: int = 1):
     if x >= 1:
         cryptopunks_offers = parse_db_offers(mongo.get_all_offers())
         cryptopunks_offers = [offer for offer in cryptopunks_offers if offer.is_valid]
         cryptopunks_offers.sort(key=lambda x: x.min_value, reverse=False)
         return {'result': 1, 'data': cryptopunks_offers[x-1]}
-    return {'result': 0}
+    return {'result': 0, 'data': 'x should be greater than or equal to 1'}
