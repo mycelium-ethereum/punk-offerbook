e3104bdfdee04d3c8bff736e79097baa8217dd95
Sumer Malhotra <sumermalhotra1998@gmail.com>
2021-10-19 18:55:04 +0800

    change API response

--- floor_price.py
+++ floor_price.py
@@ -8,9 +8,9 @@ async def root():
     try:
         cryptopunks_offers = parse_db_offers(mongo.get_all_offers())
         if len(cryptopunks_offers) != 10000:
-            return {'result': 0, 'data': 'Data is not ready. Should be ready in <1 hr.'}
+            return {'result': 0, 'data': {'error': 'Data is not ready. Should be ready in <1 hr.'}}
         cryptopunks_offers = [offer for offer in cryptopunks_offers if offer.is_valid]
         cryptopunks_offers.sort(key=lambda x: x.min_value, reverse=False)
-        return {'result': 1, 'data': cryptopunks_offers[0].min_value}
+        return {'result': 1, 'data': {'price': cryptopunks_offers[0].min_value}}
     except Exception as e:
-        return {'result': 0, 'data': 'x should be greater than or equal to 1'}
+        return {'result': 0, 'data': {'error': 'x should be greater than or equal to 1'}}
