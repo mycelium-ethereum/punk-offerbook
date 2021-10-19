871144bea6482c353c5513320e24f14be6630db4
Sumer Malhotra <sumermalhotra1998@gmail.com>
2021-10-19 15:22:11 +0800

    floor price returns floor price, not median

--- floor_price.py
+++ floor_price.py
@@ -1,4 +1,3 @@
-import numpy as np
 from utils import *
 from fastapi import FastAPI
 
@@ -12,6 +11,6 @@ async def root():
             return {'result': 0, 'data': 'Data is not ready. Should be ready in <1 hr.'}
         cryptopunks_offers = [offer for offer in cryptopunks_offers if offer.is_valid]
         cryptopunks_offers.sort(key=lambda x: x.min_value, reverse=False)
-        return {'result': 1, 'data': np.median([offer.min_value for offer in cryptopunks_offers[:4]])}
+        return {'result': 1, 'data': cryptopunks_offers[0].min_value}
     except Exception as e:
         return {'result': 0, 'data': 'x should be greater than or equal to 1'}
